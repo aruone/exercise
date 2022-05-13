28e4a3763a54e49d00d4b132fe86ad998d8ac987
aruone <47141789+aruone@users.noreply.github.com>
2022-05-13 22:43:48 +0300

    Update nok_count.py

--- nok_count.py
+++ nok_count.py
@@ -9,8 +9,7 @@ with open("events.log") as file:
         if not line.isspace() and line.split()[-1] == 'NOK':  # обрабатываем строку только если она не пустая и содержит 'NOK' в конце
             date_str = ' '.join(line.split()[0:2])  # парсим дату из строки
             date = datetime.strptime(date_str, '[%Y-%m-%d %H:%M:%S]')  # и переводим в класс datetime
-            if prev_date.replace(second=0) != date.replace(
-                    second=0):  # если предыдущая дата (без секунд) и текущая не совпадают, счетчик побнуляется
+            if prev_date.replace(second=0) != date.replace(second=0):  # если предыдущая дата (без секунд) и текущая не совпадают, счетчик обнуляется
                 c = 0
             c += 1  # счетчик событий NOK увеличивается на один
             result[date.strftime('%Y-%m-%d %H:%M')] = c  # записываем результат для текущей минуты
